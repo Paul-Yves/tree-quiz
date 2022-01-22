635b15bd554639db40f335aadfbeef52474e9e1d
PY <lucas.paulyves@gmail.com>
2022-01-22 19:49:11 +0100

    better purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.html.haml",
-      "./app/**/*.js", "./app/**/*.jsx"],
+    content: ["./app/**/*.html.haml", "./app/**/*.js", "./app/**/*.jsx"],
     options: {
       safelist: {
         greedy: ["/safe$/"],
