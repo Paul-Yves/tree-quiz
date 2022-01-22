2cc4c6119c5c32effb7ad188d034964e49dae2ec
PY <lucas.paulyves@gmail.com>
2022-01-22 19:41:21 +0100

    better tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/**/*.html.haml",
+    "./app/**/*.js",
+    "./app/**/*.jsx",
+  ],
   content: [],
   theme: {
     extend: {},
