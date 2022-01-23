25d7a97d78122472e0aa0276ca474fce467844cf
PY <lucas.paulyves@gmail.com>
2022-01-23 16:36:36 +0100

    delete answers from index view

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,22 @@
 class AnswersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, only: :create
+  before_action :check_password, only: %i[index destroy]
 
-  def index
-    return head :forbidden if params[:password] != ENV['PASSWORD']
-  end
+  def index; end
 
   def create
     Answer.create!(username: params[:name], user_answers: params[:answers])
     render json: {}
+  end
+
+  def destroy
+    Answer.delete(params[:id])
+    redirect_to(action: 'index', password: params[:password])
+  end
+
+  private
+
+  def check_password
+    return head :forbidden if params[:password] != ENV['PASSWORD']
   end
 end
